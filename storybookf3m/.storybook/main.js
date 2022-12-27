5aaeb1d60b0ea2359ccd210bad7e50547d410070
F3MDEV <f3m_github@outlook.com>
2022-12-27 17:33:24 +0000

    chore & func: f3mTopBar; mui5 setup, bootstrap,

--- storybookf3m/.storybook/main.js
+++ storybookf3m/.storybook/main.js
@@ -4,10 +4,14 @@ module.exports = {
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
+    //"@storybook/preset-create-react-app" this should be on it but has problems with typescript... letting our documentation to be incomplete.
+    //can't find a solution for it
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app"
+    '@storybook/addon-a11y',
+    '@storybook/addon-viewport',
+    'addon-screen-reader'
   ],
   "framework": "@storybook/react",
   "core": {
